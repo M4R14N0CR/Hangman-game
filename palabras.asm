6d9215a7c322657e071971c2f0b68dc7f4e5eba9
Mariano Oconitrillo Vega <99371657+M4R14N0CR@users.noreply.github.com>
2025-04-07 18:16:22 -0600

    Create palabras.asm

---
+++ palabras.asm
@@ -0,0 +1,208 @@
+section .data
+
+;--------------------------------------------------------------------------
+;------------------------PALABRAS DIFICULTAD BAJA--------------------------
+;--------------------------------------------------------------------------
+pal db "MUNDO",0
+lenpal equ $-pal
+
+copal db "MUNDO"
+lencopal equ $-copal
+
+msli db "_  _  _  _  _"
+lenmsli equ $-msli
+
+espa db " , , , , , , , , , , , , "
+lenespa equ $-espa
+;--------------------------------------------------------------------------
+
+pal1 db "ABUELO",0
+lenpal1 equ $-pal1
+
+copal1 db "ABUELO"
+lencopal1 equ $-copal1
+
+msli1 db "_  _  _  _  _  _"
+lenmsli1 equ $-msli1
+
+espa1 db " , , , , , , , , , , , , , "
+lenespa1 equ $-espa1
+;--------------------------------------------------------------------------
+
+pal2 db "ALPINOS",0
+lenpal2 equ $-pal2
+
+copal2 db "ALPINOS"
+lencopal2 equ $-copal2
+
+msli2 db "_  _  _  _  _  _  _"
+lenmsli2 equ $-msli2
+
+espa2 db " , , , , , , , , , , , , , , "
+lenespa2 equ $-espa2
+;--------------------------------------------------------------------------
+
+pal3 db "COLGANTE",0
+lenpal3 equ $-pal3
+
+copal3 db "COLGANTE"
+lencopal3 equ $-copal3
+
+msli3 db "_  _  _  _  _  _  _  _"
+lenmsli3 equ $-msli3
+
+espa3 db " , , , , , , , , , , , , , , , "
+lenespa3 equ $-espa3
+;--------------------------------------------------------------------------
+
+pal4 db "BINOCULAR",0
+lenpal4 equ $-pal4
+
+copal4 db "BINOCULAR"
+lencopal4 equ $-copal4
+
+msli4 db "_  _  _  _  _  _  _  _  _"
+lenmsli4 equ $-msli4
+
+espa4 db " , , , , , , , , , , , , , , , , "
+lenespa4 equ $-espa4
+;--------------------------------------------------------------------------
+;----------------------PALABRAS DIFICULTAD MEDIA---------------------------
+;--------------------------------------------------------------------------
+
+palM1 db "CAPITULOS",0
+lenpalM1 equ $-palM1
+
+copalM1 db "CAPITULOS"
+lencopalM1 equ $-copalM1
+
+msliM1 db "_  _  _  _  _  _  _  _  _"
+lenmsliM1 equ $-msliM1
+ 
+espaM1 db " , , , , , , , , , , , , , , , , , , , "
+lenespaM1 equ $-espaM1 
+;--------------------------------------------------------------------------
+ 
+palM2 db "NEUMATICOS",0
+lenpalM2 equ $-palM2
+
+copalM2 db "NEUMATICOS"
+lencopalM2 equ $-copalM2
+
+msliM2 db "_  _  _  _  _  _  _  _  _  _"
+lenmsliM2 equ $-msliM2
+
+espaM2 db " , , , , , , , , , , , , , , , , , , , , "
+lenespaM2 equ $-espaM2 
+;--------------------------------------------------------------------------
+
+palM3 db "MURCIELAGOS",0
+lenpalM3 equ $-palM3
+
+copalM3 db "MURCIELAGOS"
+lencopalM3 equ $-copalM3
+
+msliM3 db "_  _  _  _  _  _  _  _  _  _  _"
+lenmsliM3 equ $-msliM3
+
+espaM3 db " , , , , , , , , , , , , , , , , , , , , , "
+lenespaM3 equ $-espaM3 
+;--------------------------------------------------------------------------
+
+;--------------------------------------------------------------------------
+
+palM4 db "SUBMARINO",0
+lenpalM4 equ $-palM4
+
+copalM4 db "SUBMARINO"
+lencopalM4 equ $-copalM4
+
+msliM4 db "_  _  _  _  _  _  _  _  _ "
+lenmsliM4 equ $-msliM4
+
+espaM4 db " , , , , , , , , , , , , , , , , , , , "
+lenespaM4 equ $-espaM4 
+;--------------------------------------------------------------------------
+
+;--------------------------------------------------------------------------
+
+palM5 db "VENTILANDO",0
+lenpalM5 equ $-palM5
+
+copalM5 db "VENTILANDO"
+lencopalM5 equ $-copalM5
+
+msliM5 db "_  _  _  _  _  _  _  _  _  _"
+lenmsliM5 equ $-msliM5
+
+espaM5 db " , , , , , , , , , , , , , , , , , , , , "
+lenespaM5 equ $-espaM5
+;--------------------------------------------------------------------------
+;------------------------PALABRAS DIFICULTAD ALTA--------------------------
+;--------------------------------------------------------------------------
+
+palA1 db "CALUMBRIENTO",0
+lenpalA1 equ $-palA1
+
+copalA1 db "CALUMBRIENTO"
+lencopalA1 equ $-copalA1
+
+msliA1 db "_  _  _  _  _  _  _  _  _  _  _  _"
+lenmsliA1 equ $-msliA1
+
+espaA1 db " , , , , , , , , , , , , , , , , , , , , , , "
+lenespaA1 equ $-espaA1 
+;--------------------------------------------------------------------------
+
+palA2 db "YUXTAPONDREIS",0
+lenpalA2 equ $-palA2
+
+copalA2 db "YUXTAPONDREIS"
+lencopalA2 equ $-copalA2
+
+msliA2 db "_  _  _  _  _  _  _  _  _  _  _  _  _"
+lenmsliA2 equ $-msliA2
+
+espaA2 db " , , , , , , , , , , , , , , , , , , , , , , , "
+lenespaA2 equ $-espaA2 
+;--------------------------------------------------------------------------
+
+palA3 db "VISLUMBRANDOTE",0
+lenpalA3 equ $-palA3
+
+copalA3 db "VISLUMBRANDOTE"
+lencopalA3 equ $-copalA3
+
+msliA3 db "_  _  _  _  _  _  _  _  _  _  _  _  _  _"
+lenmsliA3 equ $-msliA3
+
+espaA3 db " , , , , , , , , , , , , , , , , , , , , , , , , "
+lenespaA3 equ $-espaA3 
+;--------------------------------------------------------------------------
+
+palA4 db "HIPERBLANDUZCOS",0
+lenpalA4 equ $-palA4
+
+copalA4 db "HIPERBLANDUZCOS"
+lencopalA4 equ $-copalA4
+
+msliA4 db "_  _  _  _  _  _  _  _  _  _  _  _  _  _  _"
+lenmsliA4 equ $-msliA4
+
+espaA4 db " , , , , , , , , , , , , , , , , , , , , , , , , "
+lenespaA4 equ $-espaA4
+
+;--------------------------------------------------------------------------
+
+palA5 db "REPUBLICANOS",0
+lenpalA5 equ $-palA5
+
+copalA5 db "REPUBLICANOS"
+lencopalA5 equ $-copalA5
+
+msliA5 db "_  _  _  _  _  _  _  _  _  _  _  _"
+lenmsliA5 equ $-msliA5
+
+espaA5 db " , , , , , , , , , , , , , , , , , , , "
+lenespaA5 equ $-espaA5
+;--------------------------------------------------------------------------
